ea5f45dc5aae36d8d4807dfa65a73f6dc1397c7f
Tdmaxer <49441022+Tdmaxer@users.noreply.github.com>
2024-06-13 16:39:34 +0530

    app.jsx setup success

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
-import React from 'react'
-import Home from './pages/Home/Home.jsx'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home/Home.jsx';
+import Login from './pages/Login/Login.jsx';
+import Register from './pages/Register/Register.jsx';
 
 function App() {
   return (
-    <div>
-      <Home />
-    </div>
-  )
+    <Router>
+      <Routes>
+        <Route path="/dashboard" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
